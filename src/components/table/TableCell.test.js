2ddd1b3295e5aeca80eef690804a7a4d05f0c8ff
Erik Raith <raithhtiar@gmail.com>
2020-11-15 19:33:27 -0700

    [test] Add TableCell test

---
+++ src/components/table/TableCell.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { TableCell } from "./TableCell";
+import { mount } from "enzyme";
+import toJson from "enzyme-to-json";
+
+describe("TableCell", () => {
+  describe("Snapshots", () => {
+    it("should match the snapshot", () => {
+      const props = {
+        data: "Fake-Data",
+      };
+      const wrapper = mount(
+        <table>
+          <tbody>
+            <tr>
+              <TableCell {...props} />
+            </tr>
+          </tbody>
+        </table>
+      );
+
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
+  });
+});
+Î
