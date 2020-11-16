424c80c55e1f9d9faee021994ce1d7a142f8a05b
Erik Raith <raithhtiar@gmail.com>
2020-11-15 20:50:12 -0700

    [test] update SearchBar Tests

--- src/components/searchBar/SearchBar.test.js
+++ src/components/searchBar/SearchBar.test.js
@@ -15,7 +15,6 @@ describe("SearchBar", () => {
   describe("user interactions", () => {
     it("should update searchValue", () => {
       const wrapper = mount(<SearchBar />);
-
       const input = wrapper.find('input[type="text"]');
 
       expect(input.props()).toEqual({
@@ -36,57 +35,40 @@ describe("SearchBar", () => {
     });
     it("should clear value when button is clicked", () => {
       const mockHandleSearchChange = jest.fn();
-
       const props = {
         handleSearchChange: mockHandleSearchChange,
       };
       const wrapper = mount(<SearchBar {...props} />);
-
       const input = wrapper.find('input[type="text"]');
-
-      expect(input.props()).toEqual({
-        id: "search",
-        onChange: expect.anything(),
-        placeholder: "Search",
-        type: "text",
-        value: "",
-      });
 
       input.instance().value = "TEST";
       input.simulate("change");
 
       expect(input.instance().value).toBe("TEST");
       expect(wrapper.find("TEST")).toBeTruthy();
+
       const clear = wrapper.find('button[type="reset"]');
       clear.simulate("click");
+
       expect(input.instance().value).toBe("");
     });
     it("should submit value when button is clicked", () => {
       const mockHandleSearchChange = jest.fn();
-
       const props = {
         handleSearchChange: mockHandleSearchChange,
       };
       const wrapper = mount(<SearchBar {...props} />);
-
       const input = wrapper.find('input[type="text"]');
-
-      expect(input.props()).toEqual({
-        id: "search",
-        onChange: expect.anything(),
-        placeholder: "Search",
-        type: "text",
-        value: "",
-      });
 
       input.instance().value = "TEST";
       input.simulate("change");
 
       expect(input.instance().value).toBe("TEST");
       expect(wrapper.find("TEST")).toBeTruthy();
-      const submit = wrapper.find('button[type="submit"]');
 
+      const submit = wrapper.find('button[type="submit"]');
       submit.simulate("submit");
+
       expect(input.instance().value).toBe("TEST");
       expect(mockHandleSearchChange).toHaveBeenCalledWith("TEST");
     });
