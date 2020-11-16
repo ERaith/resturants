ef5f044eebd766ad66ca067622ee8d297f56a9b1
Erik Raith <raithhtiar@gmail.com>
2020-11-15 18:09:15 -0700

    [wip] Add fetch for resturant data

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,30 @@
+import React, {useState,useEffect} from 'react';
+
 import Dashboard from '../pages/Dashboard';
+
 import './App.scss';
 
 function App() {
+  const [data,setData] = useState([])
+
+  async function fetchData() {
+    const res = await fetch("https://code-challenge.spectrumtoolbox.com/api/restaurants",{
+      method:"GET",
+      headers:{
+        "Content-Type":"application/json",
+        "Authorization": "Api-Key q3MNxtfep8Gt",
+      }
+    })
+    return res.json()
+    
+  }
+
+  useEffect(() => {
+    fetchData()
+    .then(data=>setData(data))
+    
+  }, [])
+
   return (
     <div className="App">
       <Dashboard/>
@@ -10,3 +33,10 @@ function App() {
 }
 
 export default App;
+
+
+// fetch(“https://code-challenge.spectrumtoolbox.com/api/restaurants”, {
+//   headers: {
+//   Authorization: “Api-Key q3MNxtfep8Gt”,
+//   },
+//  });
